040ffe7eb762cdf424b4568b20df78c37ff63c7b
akpermyakova <ak.permyakova@geekbrains.ru>
2021-02-08 00:16:17 +0300

    третья домашка

---
+++ ClassComplex/ClassComplexCmd.cs
@@ -0,0 +1,70 @@
+﻿#region Автор
+// Пермякова А.К.
+#endregion
+
+#region Описание задачи
+/*
+
+б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+
+*/
+#endregion
+
+//#define Plus
+//#define Multi
+#define Minus
+
+using System;
+
+namespace StructComplex
+{
+    class StructComplexCmd
+    {
+        static void Main(string[] args)
+        {
+            SComplex complex1;
+            complex1.Re = 1;
+            complex1.Im = 1;
+
+            SComplex complex2;
+            complex2.Re = 2;
+            complex2.Im = 2;
+
+#if Plus
+            Complex result = complex1.Plus(complex2);
+#elif Multi
+            Complex result = complex1.Multi(complex2);
+#elif Minus
+            SComplex result = complex1.Minus(complex2);
+#endif
+            Console.WriteLine(result.AsString());
+            Console.ReadKey();
+        }
+    }
+
+    public class CComplex
+    {
+        int Re;
+        int Im;
+
+        public CComplex()
+        {
+            Re = 0;
+            Im = 0;
+        }
+
+        public CComplex(int _Im, int Re)
+        {
+            Im = _Im;
+            this.Re = Re;
+        }
+
+        public CComplex Plus(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.Im = x2.Im + Im;
+            x3.Re = x2.Re + Re;
+            return x3;
+        }   
+    }
+}
---
+++ StructComplex/StructComplexCmd.cs
@@ -0,0 +1,85 @@
+﻿#region Автор
+// Пермякова А.К.
+#endregion
+
+#region Описание задачи
+/*
+
+1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.
+Продемонстрировать работу структуры;
+
+*/
+#endregion
+
+//#define Plus
+//#define Multi
+#define Minus
+
+using System;
+
+namespace StructComplex
+{
+    class StructComplexCmd
+    {
+        static void Main(string[] args)
+        {
+            SComplex complex1;
+            complex1.Re = 1;
+            complex1.Im = 1;
+
+            SComplex complex2;
+            complex2.Re = 2;
+            complex2.Im = 2;
+
+#if Plus
+            Complex result = complex1.Plus(complex2);
+#elif Multi
+            Complex result = complex1.Multi(complex2);
+#elif Minus
+            SComplex result = complex1.Minus(complex2);
+#endif
+            Console.WriteLine(result.AsString());
+            Console.ReadKey();
+        }
+    }
+
+    struct SComplex
+    {
+        public int Im;
+        public int Re;
+        
+        public SComplex Plus(SComplex x)
+        {
+            SComplex y;
+            y.Im = Im + x.Im;
+            y.Re = Re + x.Re;
+            return y;
+        }
+
+        public SComplex Multi(SComplex x)
+        {
+            SComplex y;
+            y.Im = Re * x.Im + Im * x.Re;
+            y.Re = Re * x.Re - Im * x.Im;
+            return y;
+        }
+
+        public SComplex Minus(SComplex x)
+        {
+            SComplex y;
+            y.Im = Im - x.Im;
+            y.Re = Re - x.Re;
+            return y;
+        }
+
+        public string AsString()
+        {
+            if(Im == 0) return ($"{Re}");
+            if(Re == 0) return ($"{Im}i");
+            if(Re == 0 && Im ==0) return ($"0");
+            if (Im < 0) return ($"{Re}{Im}i");
+            else return ($"{Re}+{Im}i");
+
+        }
+    }
+}
