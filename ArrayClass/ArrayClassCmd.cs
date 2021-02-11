36ce6cf8ebc9e056fbd6791f4fb1283169faaf12
akpermyakova <ak.permyakova@geekbrains.ru>
2021-02-12 01:15:24 +0300

    4 дз

---
+++ ArrayClass/ArrayClassCmd.cs
@@ -0,0 +1,90 @@
+﻿#region Автор
+// Пермякова А.К.
+#endregion
+
+#region Описание задачи
+/*
+
+2. а) Дописать класс для работы с одномерным массивом.
+Реализовать конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом.
+Создать свойство Sum, которые возвращают сумму элементов массива, метод Inverse, меняющий знаки у всех элементов массива, метод Multi,
+умножающий каждый элемент массива на определенное число, свойство MaxCount, возвращающее количество максимальных элементов.
+В Main продемонстрировать работу класса.
+б)Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+
+*/
+#endregion
+
+using System;
+
+namespace ArrayClass
+{
+    class ArrayClassCmd
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите размер массива:");
+                var count = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите стартовое значение:");
+                var startValue = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите шаг значений:");
+                var step = int.Parse(Console.ReadLine());
+
+                var myArray = new MyArray(count, startValue, step);
+                var array = myArray.CreateArray();
+
+                Console.Write("Текущий массив:");
+                Console.WriteLine();
+
+                foreach(var mass in array)
+                {
+                    Console.Write(mass + " ");
+                }
+
+                var sum = array;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    class MyArray
+    {
+        int Count;
+        int StartValue;
+        int Step;
+
+        public MyArray(int count, int startValue, int step)
+        {
+            Count = count;
+            StartValue = startValue;
+            Step = step;
+        }
+
+        public int[] CreateArray()
+        {
+            var massive = new int[Count];
+            var value = StartValue;
+            for (var i = 0; i < Count; i++)
+            {
+                if (i == 0) massive[i] = StartValue;
+                else
+                {
+                    value += Step;
+                    massive[i] = value;
+                }
+            }
+            return massive;
+        }
+    }
+}
---
+++ ArrayPairs/ArrayPairsCmd.cs
@@ -0,0 +1,64 @@
+﻿#region Автор
+// Пермякова А.К.
+#endregion
+
+#region Описание задачи
+/*
+
+1. Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых хотя бы одно число делится на 3.
+В данной задаче под парой подразумевается два подряд идущих элемента массива.
+Например, для массива из пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.
+
+*/
+#endregion
+
+using System;
+
+namespace ArrayPairs
+{
+    class ArrayPairsCmd
+    {
+        static void Main(string[] args)
+        {
+            var massive = new int[20];
+            var random = new Random();
+            var pairCount = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                massive[i] = random.Next(-10000, 10001);
+            }
+            
+            for(int i = 0; i<20; i++)
+            {
+                if (i != 19)
+                {
+                    if (massive[i] % 3 == 0 || massive[i + 1] % 3 == 0) pairCount++;
+                }
+                else break;
+                
+            }
+
+            Console.Write("В массиве:");
+            Console.WriteLine();
+
+            foreach (var mass in massive)
+            {
+                if (GetSumOfDigits(mass) % 3 == 0) Console.Write($"{mass}({true}) ");
+                else Console.Write($"{mass}({false}) ");
+            }
+
+            Console.WriteLine();
+            Console.Write($"{pairCount} пар чисел, в которых хотя бы одно значение кратно 3");
+            
+            Console.ReadKey();
+        }
+
+        static int GetSumOfDigits(int value)
+        {
+            if (value == 0) return 0;
+            else return GetSumOfDigits(value / 10) + value % 10;
+        }
+    }
+}
